85f6af8821e3513e75ea7b0663d3f69cca5063c7
Alexandros T. Ioannidis ♐ <alexioanid@yahoo.gr>
2020-09-21 08:27:19 +0100

    Update exercise3.py

--- exercise3.py
+++ exercise3.py
@@ -31,3 +31,28 @@ class Interval:
             return False
         
         return(False)
+    
+    if __name__ == '__main__':
+        print('Παρακαλώ εισάγετε τον αριθμό')
+        N = input()
+        tempList = []
+        for i in range(N):
+            a = random.uniform(-2.0, 2.99)
+            b = random.uniform(-2.0, 2.99)
+            tempList.append([a,b])
+            
+       cnt1 = 0
+       cnt2 = 0
+        
+        for idx, i in enumerate(tempList):
+            interval1 = Interval(i[0], i[1])
+            other = Interval(0,0)
+            
+            if interval1.intersects(other) == True:
+                cnt1 += 1
+                
+            if interval1.contains(0) == True:
+                cnt2 += 1
+                
+       print(cnt1, " διαστήματα τέμνονται από το διάστημα 0")
+       print(cnt2, " διαστήματα περιέχουν το 0")
