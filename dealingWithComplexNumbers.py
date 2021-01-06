c9be6608570fe40ae90606f4e376e070ad626f55
Alexandros T. Ioannidis ♐  (°.°) <alexioanid@yahoo.gr>
2021-01-06 14:20:31 +0200

    Create dealingWithComplexNumbers.py

---
+++ dealingWithComplexNumbers.py
@@ -0,0 +1,16 @@
+'''
+You are given two complex numbers, and you have to print the result of their addition, subtraction, multiplication, division and modulus operations.
+The real and imaginary precision part should be correct up to 2 decimal places.
+
+Sample Input:
+2 1
+5 6
+
+Sample Output
+7.00+7.00i
+-3.00-5.00i
+4.00+17.00i
+0.26-0.11i
+2.24+0.00i
+7.81+0.00i
+'''
