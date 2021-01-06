41f15cc24cc72993f923ef1a3e341c26a35d2689
Alexandros T. Ioannidis ♐  (°.°) <alexioanid@yahoo.gr>
2021-01-06 13:10:47 +0200

    Update regexSubstitutionTask.py

--- regexSubstitutionTask.py
+++ regexSubstitutionTask.py
@@ -1,1 +1,50 @@
+'''
+import re
 
+#Squaring numbers
+def square(match):
+    number = int(match.group(0))
+    return str(number**2)
+
+print re.sub(r"\d+", square, "1 2 3 4 5 6 7 8 9")
+
+Output: 
+1 4 9 16 25 36 49 64 81
+------------------------------------------------------------
+import re
+
+html = """
+<head>
+<title>HTML</title>
+</head>
+<object type="application/x-flash" 
+  data="your-file.swf" 
+  width="0" height="0">
+  <!-- <param name="movie"  value="your-file.swf" /> -->
+  <param name="quality" value="high"/>
+</object>
+"""
+
+print re.sub("(<!--.*?-->)", "", html) #remove comment
+
+Output:
+<head>
+<title>HTML</title>
+</head>
+<object type="application/x-flash" 
+  data="your-file.swf" 
+  width="0" height="0">
+
+  <param name="quality" value="high"/>
+</object>
+------------------------------------------------------------
+Task
+
+You are given a text of N lines. The text contains && and || symbols.
+Your task is to modify those symbols to the following:
+
+&& → and
+|| → or
+
+Both && and || should have a space " " on both sides.
+'''
